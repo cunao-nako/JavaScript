4a42eecfc3d63073dcc20d4b9ecbc4839e196543
Igor <u1e_e1e@mail.ru>
2021-04-07 20:21:30 +0300

    app v0.2

---
+++ Udemy/Полный курс по JavaScript + React/14/14.js
@@ -0,0 +1,6 @@
+let number = 50;
+
+while (number <= 55) {
+    console.log(number);
+    number++;
+}
--- Udemy/Полный курс по JavaScript + React/app/app.js
+++ Udemy/Полный курс по JavaScript + React/app/app.js
@@ -8,10 +8,39 @@ let personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
+// let i = 0;
+// while (i < 2) {
+//   let movie = prompt('Один из последних фильмов что вы посмотрели?', '');
+//   let score = prompt('На сколько оцените его от 0 до 10?', '');
+//   if (movie && score && movie.length < 50) {
+//     personalMovieDB.movies[movie] = score;
+//     i++;
+//   }
+// }
+
+// let i = 0;
+// do {
+//   let movie = prompt('Один из последних фильмов что вы посмотрели?', '');
+//   let score = prompt('На сколько оцените его от 0 до 10?', '');
+//   if (movie && score && movie.length < 50) {
+//     personalMovieDB.movies[movie] = score;
+//     i++;
+//   }
+// } while(i < 2);
+
+for (let i = 0; i < 2;) {
   let movie = prompt('Один из последних фильмов что вы посмотрели?', '');
   let score = prompt('На сколько оцените его от 0 до 10?', '');
-  personalMovieDB.movies[movie] = score;
+  if (movie && score && movie.length < 50) {
+    personalMovieDB.movies[movie] = score;
+    i++;
+  }
 }
+
+if (personalMovieDB.count < 10) {console.log('Вы посмотрели мало фильмов!');}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {console.log('Вы классный зритель');}
+else if (personalMovieDB.count >= 30) {console.log('Вы киноман!');}
+else {console.log('Произошка ошибка');}
+
 
 console.log(personalMovieDB);
