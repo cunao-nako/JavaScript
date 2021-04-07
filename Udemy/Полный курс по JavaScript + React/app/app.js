23bd1298bcaa2f07532a5ef53c2f241116ea463c
Igor <u1e_e1e@mail.ru>
2021-04-07 19:06:40 +0300

    app v0.1

--- Udemy/Полный курс по JavaScript + React/app/app.js
+++ Udemy/Полный курс по JavaScript + React/app/app.js
@@ -1,1 +1,17 @@
-let message = 'Я очень сильно люблю Cонечку';
+let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  let movie = prompt('Один из последних фильмов что вы посмотрели?', '');
+  let score = prompt('На сколько оцените его от 0 до 10?', '');
+  personalMovieDB.movies[movie] = score;
+}
+
+console.log(personalMovieDB);
