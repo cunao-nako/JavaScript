32e588ca0c7c25c5df0b777097b6bcd566ff36de
Igor <u1e_e1e@mail.ru>
2021-04-12 05:11:01 +0300

    new exercises

--- Udemy/Полный курс по JavaScript + React/39/39.js
+++ Udemy/Полный курс по JavaScript + React/39/39.js
@@ -1,11 +1,53 @@
-let button = document.querySelector('.btn');
-let timer;
-button.addEventListener('click', () => {
-  timer = setTimeout(logger, 20);
+let button = document.querySelector('.btn'),
+    stopButton = document.querySelector('.stop'),
+    element = document.querySelector('.box'),
+    counter = 0,
+    side = 1,
+    animationInterval,
+    animationBuffer = [];
+
+stopButton.addEventListener('click', () => {
+  animationBuffer.forEach(item => clearInterval(item));
 });
 
-clearInterval(timer);
+button.addEventListener('click', () => {
+  animationBuffer.forEach(item => clearInterval(item));
+  animationInterval = setInterval(animation, 4);
+  animationBuffer.push(animationInterval);
+});
 
-function logger() {
-  console.log('Hi');
+function animation() {
+  if(counter <= 300 && side === 1) {
+    element.style.top = counter + 'px';
+    element.style.bottom = counter + 'px';
+    counter++;
+  }
+  if (counter === 300 && side === 1) {
+    side++;
+    counter = 0;
+  }
+  if (counter <= 300 && side === 2) {
+    element.style.bottom = counter + 'px';
+    element.style.left = counter + 'px';
+    counter++;
+  }
+  if (counter === 300 && side === 2) {
+    side++;
+    counter = 300;
+  }
+  if (counter >= 0 && side === 3) {
+    element.style.top = counter + 'px';
+    element.style.bottom = counter + 'px';
+    counter--;
+  }
+  if (counter === 0 && side === 3) {
+    side++;
+    counter = 300;
+  }
+  if (counter >= 0 && side === 4) {
+    element.style.top = '0px';
+    element.style.left = counter + 'px';
+    counter--;
+  }
+  if (counter === 0 && side === 4) { side = 1; }
 }
