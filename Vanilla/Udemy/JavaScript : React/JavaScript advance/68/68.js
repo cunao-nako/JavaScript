8e327c13d9ffd35842cc14b27113aa9b4e51d974
Igor Nikonov <u1e_e1e@mail.ru>
2021-05-11 12:22:19 +0300

    ы

--- Vanilla/Udemy/JavaScript : React/JavaScript advance/68/68.js
+++ Vanilla/Udemy/JavaScript : React/JavaScript advance/68/68.js
@@ -1,1 +1,23 @@
-console.log('hi');
+// инкапсуляция в функции-конструкторе
+function User(name, surname) {
+  let userName = name,
+    userSurname = surname;
+
+  this.say = text => text +', with pleasure, ' + userName + ' ' + userSurname;
+
+  this.getName  = () => { return userName; };
+  this.setName = (name) => {
+    if (isNaN(name)) { userName = name; }
+    else { console.log('Inncorect name!'); }
+  };
+}
+
+let user1 = new User('Alex', 'Miller');
+console.log(user1.say('hi'));
+
+
+user1.userName = 'J';
+user1.setName('Jhon');
+console.log(user1.say('hi'));
+
+// инкапсуляция в классе
