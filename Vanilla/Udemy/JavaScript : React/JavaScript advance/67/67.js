36eeebae0921b9622de626a27dfb935615df0095
Igor Nikonov <u1e_e1e@mail.ru>
2021-05-11 11:58:43 +0300

    new

--- Vanilla/Udemy/JavaScript : React/JavaScript advance/67/67.js
+++ Vanilla/Udemy/JavaScript : React/JavaScript advance/67/67.js
@@ -1,3 +1,16 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('hi');
-});
+// акцессоры
+const user = {
+  name: 'Jhon',
+  surname: 'Smith',
+  age: 25,
+  get userData() { return this.name + ' ' + this.surname + ', ' + this.age + ' age old.'; },
+  set userData(data) { this.name = data.name; this.surname = data.surname; this.age = data.age; },
+},
+data = {
+  name: 'Alex',
+  surname: 'Miller',
+  age: 35,
+}
+
+user.userData = data;
+console.log(user.userData);
